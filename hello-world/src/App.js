9a73fd97c05287bd51929b3d060bf4309d9470f0
Abhishek Marubadi <47182945+abhim5@users.noreply.github.com>
2024-01-31 13:11:27 -0600

    Daily Learnings

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,5 +1,8 @@
 // import logo from './logo.svg';
 import './App.css';
+import PureComp from './components/PureComp';
+// import Table from './components/Table';
+import ParentComp from './ParentComp';
 // import Greet from './components/Greet';
 // import Welcome from './components/Welcome';
 // import Hello from './components/Hello';
@@ -9,12 +12,14 @@ import './App.css';
 // import Counter from './components/Counter';
 // import Binding from './components/Binding';
 // import ParentComponent from './components/ParentComponent';
-import UserGreeting from './components/UserGreeting';
+// import UserGreeting from './components/UserGreeting';
+
 function App() {
   return (
     <div className="App">
-
-      <UserGreeting />
+      <ParentComp />
+      {/* <Table /> */}
+      {/* <UserGreeting /> */}
       {/* <ParentComponent /> */}
       {/* <Binding /> */}
       {/* <FunctionClick />
---
+++ hello-world/src/ParentComp.js
@@ -0,0 +1,36 @@
+import React, { Component, PureComponent } from 'react'
+import MemoComp from './components/MemoComp'
+import PureComp from './components/PureComp'
+import RegularComp from './RegularComp'
+
+class ParentComp extends PureComponent {
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+         name: 'Abhishek'
+      }
+    }
+
+    componentDidMount() {
+        setInterval(() => {
+            this.setState({
+                name: 'Abhishek'
+            })
+        }, 2000)
+    }
+    
+  render() {
+      console.log('******************** Parent Comp ********************')
+    return (
+      <div>
+        Parent Component
+        <MemoComp name={this.state.name} />
+        {/* <RegularComp name={this.state.name} />
+        <PureComp name={this.state.name} /> */}
+      </div>
+    )
+  }
+}
+
+export default ParentComp
