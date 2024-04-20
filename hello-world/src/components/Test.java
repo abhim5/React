8e8e71a2757d69f6e965713dbcd5c9bfee34dbb6
Abhishek Marubadi <47182945+abhim5@users.noreply.github.com>
2024-04-20 17:08:32 -0500

    Removed java file

--- hello-world/src/components/Test.java
+++
@@ -1,9 +0,0 @@
-class Test {
-    public static void main(String[] args) {
-        int n = 5;
-        for(int i=1;i<=10;i++) {
-            System.out.println(n +" X "+i+" = "+n*i);
-        }
-    }
-    
-}
